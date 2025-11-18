36f9981fa9d760a6b87a665a7cd2e9e3d33fa0ec
mohamedelasri202 <el7994995@gmail.com>
2025-11-18 13:50:38 +0000

    adding the structure and the code for the dto for the request and the response of the user and also the entity

---
+++ src/main/java/com/logistics/Smangement/dto/auth/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.logistics.Smangement.dto.auth;
+
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank(message = "Login is required")
+    private String login;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
