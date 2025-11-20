4f14bcde3d2d2785fd48840859c8d271002ba0a0
mohamedelasri202 <el7994995@gmail.com>
2025-11-20 19:35:29 +0100

    setupp the structure of the project

---
+++ src/main/java/com/example/smartmanagement/SmartManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smartmanagement/SmartManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
